4adba387bc7d5210cc39051945dbd231201495c2
William Spada <williamrspada02@gmail.com>
2025-11-24 22:31:18 -0300

    refactor: replace method patch to put

--- src/main/java/com/gametracker/gametracker_api/controller/AchievementController.java
+++ src/main/java/com/gametracker/gametracker_api/controller/AchievementController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,10 +47,10 @@ public class AchievementController {
         return service.unbindFromGame(achievementId);
     }
 
-    // Partial update Achievement (PATCH)
-    @PatchMapping("/{id}")
+    // Update Achievement (PUT)
+    @PutMapping("/{id}")
     public ResponseEntity<?> updatePartial(@PathVariable Long id, @RequestBody Achievement achievement) {
-        return service.updatePartial(id, achievement);
+        return service.update(id, achievement);
     }
 
     // Delete Achievement
--- src/main/java/com/gametracker/gametracker_api/controller/GameController.java
+++ src/main/java/com/gametracker/gametracker_api/controller/GameController.java
@@ -6,9 +6,9 @@ import java.util.Set;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,10 +72,10 @@ public class GameController {
         return service.getGamePlatforms(gameId);
     }
 
-    // Partial update Game (PATCH)
-    @PatchMapping("/{id}")
-    public ResponseEntity<?> updatePartial(@PathVariable Long id, @RequestBody Game game) {
-        return service.updatePartial(id, game);
+    // Update Game (PUT)
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Game game) {
+        return service.update(id, game);
     }
 
     // Delete Game
